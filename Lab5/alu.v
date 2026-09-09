27284938700d1227bba83afce9ad2692ae40d3fb
hatemzzz <149408767+hatemzzz@users.noreply.github.com>
2026-09-09 17:44:03 +0300

    Add files via upload

---
+++ Lab5/alu.v
@@ -0,0 +1,23 @@
+module alu#(parameter WIDTH = 8)(
+    input reg [WIDTH - 1 : 0 ]in_a , in_b ,
+    input reg [2:0] opcode,
+    output reg [WIDTH - 1 : 0 ] alu_out,
+    output wire a_is_zero 
+);
+assign a_is_zero = in_a ? 1'b0:1'b1;
+always @(*) begin
+    case (opcode)
+        3'b000: alu_out = in_a;
+        3'b001: alu_out = in_a;
+        3'b010: alu_out = in_a + in_b;
+        3'b011: alu_out = in_a & in_b;
+        3'b100: alu_out = in_a ^ in_b;
+        3'b101: alu_out = in_b;
+        3'b110: alu_out = in_a;
+        3'b111: alu_out = in_a;
+
+        default: alu_out = {WIDTH{1'b0}};
+    endcase
+end
+
+endmodule
